bba8452d87eef8ab23b3235816e46818bb2a5d7f
John Rees <johnrees@users.noreply.github.com>
2021-06-12 22:01:55 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -22,7 +22,7 @@ const rateLimitedFetch = async (url, options = {}) => {
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
-  if (url.host === "devnet.solana.com") {
+  if (url.host.endsWith("solana.com")) {
     event.respondWith(
       rateLimitedFetch(event.request).then((response) => {
         return response;
