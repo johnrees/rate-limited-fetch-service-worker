244dd1e60c99d4736fd83c9968da6bba61fedd03
John Rees <john@bitsushi.com>
2021-12-23 14:19:51 +0000

    basic example

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const MS_DELAY = 100;
+const MS_DELAY = 0;
 let count = 0;
 
 const sleep = () => {
@@ -12,21 +12,56 @@ const sleep = () => {
   );
 };
 
-const rateLimitedFetch = async (url, options = {}) => {
-  // wait an amount of time that is based on how many
-  // requests are yet to finish
-  await sleep();
-  return fetch(url, options);
-};
+function debounceAgg(fn, wait = 200) {
+  let timeout;
+  let update = [];
+
+  function processUpdate() {
+    timeout = null;
+    fn(update);
+    update = [];
+  }
+
+  return (value) => {
+    update.push(value);
+    if (timeout == null) timeout = setTimeout(processUpdate, wait);
+  };
+}
+
+const batchedFetch = (url, options = {}) =>
+  new Promise(async (resolve) => {
+    await sleep();
+    foo({
+      resolve,
+      url,
+      options,
+    });
+  });
+
+const foo = debounceAgg(async (args) => {
+  const body = await Promise.all(args.map((arg) => arg.url.json()));
+  const { url, options, resolve, ...rest } = args[0];
+
+  const response = await fetch(url, {
+    ...rest,
+    ...options,
+    body: JSON.stringify(body),
+  });
+  const results = await response.json();
+  console.log({ results });
+  results.forEach((result, i) => {
+    args[i]?.resolve(new Response(JSON.stringify(result), response));
+  });
+});
 
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
-
-  if (url.host.endsWith("solana.com")) {
+  // if (url.host.endsWith("solana.com")) {
+  if (url.href.includes("data.json")) {
     event.respondWith(
-      rateLimitedFetch(event.request).then((response) => {
+      batchedFetch(event.request).then((response) => {
+        console.log({ response });
         return response;
-        // TODO: retry x number of times if !response
       })
     );
   }
